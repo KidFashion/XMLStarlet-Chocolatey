be2533656d399ef7facdd1ca9c507c580fb15e75
Angelo Scotto <scotto.a@gmail.com>
2014-06-25 18:22:25 +0200

    Upload of version 1.5.0 of Xml Starlet package

---
+++ xmlstarlet.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿try {
+  $package = 'xmlstarlet'
+  $PackageVersion = "1.5.0"
+  $url = "http://sourceforge.net/projects/xmlstar/files/xmlstarlet/$PackageVersion/xmlstarlet-$PackageVersion-win32.zip/download"
+  $destination = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+
+  Install-ChocolateyZipPackage $package -url $url -unzipLocation $destination
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
